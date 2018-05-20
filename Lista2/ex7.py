e6eac77478d548f609258d173b0ffff304a50d00
Bruno Cozendey <brunocozendey@users.noreply.github.com>
2018-05-20 00:39:03 -0300

    Create ex7.py

---
+++ Lista2/ex7.py
@@ -0,0 +1,35 @@
+# -*- coding: cp1252 -*-
+'''
+Esse programa criptografa e decriptografa uma mensagem seguindo o seguinte padrão:
+Soma 2 ao código ascci de cada letra ou caracter.
+E no final exibi o texto lido, criptografado e decriptografado.
+'''
+def ler():
+    while True:
+        try:    
+            frase = raw_input('Digite a frase que será criptografada: \n')
+            break
+        except:
+            print 'Oops saiu algo errado!'
+    return frase
+
+def cript(frase):
+    frase_cript = ''
+    for i in frase:
+        frase_cript += chr(ord(i)+2)
+    return frase_cript
+
+def decript(frase):
+    frase_decript = ''
+    for i in frase:
+        frase_decript += chr(ord(i)-2)
+    return frase_decript
+
+def impr(frase,frase_cript,frase_decript):
+    print 'A sua string é \'{:1s}\'. \n Criptografada: {:1s}. \n Decriptografada:{:1s}.'.format(frase,frase_cript,frase_decript)
+
+#Main
+frase = ler()
+frase_cript = cript(frase)
+frase_decript = decript(frase_cript)
+impr(frase, frase_cript,frase_decript)
